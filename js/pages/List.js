82cec9ebcbf7ba945789bba44884f978cd644555
blazer913 <blazeyah557@gmail.com>
2026-02-15 12:42:10 +0100

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -118,7 +118,7 @@ export default {
                         Do not use secret routes or bug routes
                     </p>
                     <p>
-                        Do not use easy modes, only a record of the unmodified level qualifies
+                        Only levels that one of the DBM members have beaten are qualified to be placed.
                     </p>
                     <p>
                         Once a level falls onto the Legacy List, we accept records for it for 24 hours after it falls off, then afterwards we never accept records for said level
